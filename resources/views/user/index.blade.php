12af6d07da0c863a0d47caa41b0d76340612584a
bagusprasetyoo <bagusprstyoo11@gmail.com>
2024-01-20 18:49:04 +0800

    Menyembunyikan Beberapa Atribut

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -33,21 +33,21 @@
                                     <th>No Telpon</th>
                                     <th>KIS</th>
                                     <th>KJP</th>
-                                    <th>Suku</th>
+                                    {{-- <th>Suku</th> --}}
                                     {{-- <th>No Sepatu Safety</th> --}}
                                     {{-- <th>Start Work User</th> --}}
                                     {{-- <th>End Work User</th> --}}
-                                    <th>Loc Kerja</th>
-                                    <th>Loc</th>
+                                    {{-- <th>Loc Kerja</th> --}}
+                                    {{-- <th>Loc</th> --}}
                                     {{-- <th>Sistem Absensi</th> --}}
                                     {{-- <th>Latitude</th> --}}
                                     {{-- <th>Longitude</th> --}}
                                     {{-- <th>Aktual Cuti</th> --}}
                                     <th>Status Pernikahan</th>
-                                    <th>Istri Suami</th>
+                                    {{-- <th>Istri Suami</th>
                                     <th>Anak 1</th>
                                     <th>Anak 2</th>
-                                    <th>Anak 3</th>
+                                    <th>Anak 3</th> --}}
                                     {{-- <th>Access By</th> --}}
                                     {{-- <th>Image Url</th> --}}
                                     <th>Role App</th>
@@ -75,21 +75,21 @@
                                         <td>{{ $user->no_telpon }}</td>
                                         <td>{{ $user->kis }}</td>
                                         <td>{{ $user->kpj }}</td>
-                                        <td>{{ $user->suku }}</td>
+                                        {{-- <td>{{ $user->suku }}</td> --}}
                                         {{-- <td>{{ $user->no_sepatu_safety }}</td> --}}
                                         {{-- <td>{{ $user->start_work_user }}</td> --}}
                                         {{-- <td>{{ $user->end_work_user }}</td> --}}
-                                        <td>{{ $user->loc_kerja }}</td>
-                                        <td>{{ $user->loc }}</td>
+                                        {{-- <td>{{ $user->loc_kerja }}</td> --}}
+                                        {{-- <td>{{ $user->loc }}</td> --}}
                                         {{-- <td>{{ $user->sistem_absensi }}</td> --}}
                                         {{-- <td>{{ $user->latitude }}</td> --}}
                                         {{-- <td>{{ $user->longitude }}</td> --}}
                                         {{-- <td>{{ $user->aktual_cuti }}</td> --}}
                                         <td>{{ $user->status_pernikahan }}</td>
-                                        <td>{{ $user->istri_suami }}</td>
+                                        {{-- <td>{{ $user->istri_suami }}</td>
                                         <td>{{ $user->anak_1 }}</td>
                                         <td>{{ $user->anak_2 }}</td>
-                                        <td>{{ $user->anak_3 }}</td>
+                                        <td>{{ $user->anak_3 }}</td> --}}
                                         {{-- <td>{{ $user->access_by }}</td> --}}
                                         {{-- <td>{{ $user->image_url }}</td> --}}
                                         <td>{{ $user->role_app }}</td>
